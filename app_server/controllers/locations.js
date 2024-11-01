416f8ec0919d055a2b7dbe1573a377451370ada0
rmsgk1381 <55736746+rmsgk1381@users.noreply.github.com>
2024-11-02 03:12:12 +0900

    Update locations.js - apiOptions.server

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ const apiOptions = {
     server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-    apiOptions.server  = 'https://yourapi.com';
+    apiOptions.server  = 'https://loc8r-api-jhcs.onrender.com';
 }
 
 const homelist = (req, res) => {
