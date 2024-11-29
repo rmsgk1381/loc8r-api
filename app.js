4efd7ae7c14297d407a570e0baaac9b1f2881f46
rmsgk1381 <55736746+rmsgk1381@users.noreply.github.com>
2024-11-30 05:06:47 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,29 +4,59 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// require('./app_server/models/db');
+const passport = require('passport'); // Required before the database models
 require('./app_api/models/db');
+require('./app_api/config/passport'); // Required after the database models
 
-const indexRouter = require('./app_server/routes/index');
-// const usersRouter = require('./app_server/routes/users');
+// const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
 
-var app = express();
+const app = express();
 
-// view engine setup
+
+const cors = require('cors');
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+app.use(cors(corsOptions));
+
+app.use('/api', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
+  next();
+});
+
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build/browser')));
+app.use(passport.initialize());
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+// app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use('/users', usersRouter);
 
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'app_public', 'build/browser', 'index.html'));
+// });
+
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
+    res.sendFile(path.join(__dirname, 'app_public', 'build/browser', 'index.html'));
+});
+// error handlers
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message": err.name + ": " + err.message});
+  }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
